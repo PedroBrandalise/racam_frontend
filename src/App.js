22d009d4872b931c6805fc68edd9969060bd9a86
Pedro Brandalise <pedroaugustopb@gmail.com>
2020-06-23 14:24:22 -0300

    template da pagina de login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter} from 'react-router-dom'
 import Header from './Header'
 import './global.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 import Routes from "./routes"
@@ -13,7 +14,10 @@ function App() {
         <div className='content'>
           <Routes />
         </div>
+        
+        
       </BrowserRouter>
+      
     
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,45 @@
 
 import React, { Component} from 'react'
-
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import './Login.css'
 
 class Login extends Component {
     render (){
         return(
             <div>
-                <h1> Login</h1>
-                <p>
-                    fsdfsd
-                </p>
+                
+                
+                <Card style={{ width: '18rem' }} className="cardLogin">
+                    
+                    <Card.Body>
+                        <Card.Title>Login</Card.Title>
+                        <Card.Text>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>E-mail</Form.Label>
+                                <Form.Control type="email" placeholder="E-mail" />
+                                
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control type="password" placeholder="Senha" />
+                            </Form.Group>
+
+                            
+                            <Button variant="primary" type="submit">
+                                Enviar
+                            </Button>
+                            </Form>
+                        
+                        </Card.Text>
+                        
+                    </Card.Body>
+                </Card>
+                    
+                
             </div>
         );
     }
