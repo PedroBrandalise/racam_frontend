8c23bf66fe703d3820e83f3e605fb82a1d9cb975
Pedro Brandalise <pedroaugustopb@gmail.com>
2020-09-27 15:08:39 -0300

    mostrar photos

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -3,29 +3,50 @@
 import React, { Component} from 'react'
 import Card from 'react-bootstrap/Card'
 import './Photos.css'
+import Axios from 'axios'
 
 class Photos extends Component {
+    state = {
+        photos: [],
+    }
+    async componentDidMount(){
+        Axios({
+            method: "GET",
+            withCredentials: true,
+            url: "http://localhost:8080/photos",
+          }).then((res) => {
+            
+            this.setState({photos: res.data})
+            console.log(this.state.photos)
+            // if(res.data.loged){
+
+            // }else{
+            //     console.log('au')
+            // }
+          });
+    }
+
     render (){
         return(
             <div>
-                <Card className='cardPhoto' >
-                <Card.Img className='cardImg' src="https://tudorondonia.com/uploads/23-10-19-fwg0kkrl6u750v5.jpg "/>
-                <Card.Text className='cardText'>
-                Camera: Floresta (#1345) <br/>
-                Hora: 23:30
-                </Card.Text>
-                
-                </Card>
-                <br/>
 
-                <Card className='cardPhoto' >
-                <Card.Img className='cardImg' src="https://tudorondonia.com/uploads/23-10-19-fwg0kkrl6u750v5.jpg "/>
-                <Card.Text className='cardText'>
-                Camera: Floresta (#1345) <br/>
-                Hora: 23:30
-                </Card.Text>
+                {this.state.photos.map(photo =>(
+                    <div> 
+                    <Card className='cardPhoto' >
+                        <Card.Img className='cardImg' src={"http://localhost:8080/files/"+photo.url_photo}/>
+
+                        <Card.Text className='cardText'>
+                        { 
+                            //Camera:  <br/> }
+                        }
+                        Hora: {Date(photo.hour)}
+                        </Card.Text>
+                    </Card>
+                    <br/>
+                    </div>
+                    
+                ))}
                 
-                </Card>
             </div>
         );
     }
